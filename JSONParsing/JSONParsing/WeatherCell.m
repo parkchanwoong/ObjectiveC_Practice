60834240a1902ac56b5493216a1f1bc813f74744
Chanwoong Park <foodi1121@gmail.com>
2017-09-22 15:33:24 +0900

    Add JsonParsing

---
+++ JSONParsing/JSONParsing/WeatherCell.m
@@ -0,0 +1,23 @@
+//
+//  WeatherCell.m
+//  JSONParsing
+//
+//  Created by ChoiJinYoung on 3/6/16.
+//  Copyright © 2016 appstamp. All rights reserved.
+//
+
+#import "WeatherCell.h"
+
+@implementation WeatherCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
