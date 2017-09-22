009e7c9a66dc9ad00c3185be7ee47d1e3ef7052b
Chanwoong Park <foodi1121@gmail.com>
2017-09-22 15:44:14 +0900

    Add XMLParsing

---
+++ Source/XMLParsing/XMLParsing/WeatherCell.h
@@ -0,0 +1,18 @@
+//
+//  WeatherCell.h
+//  XMLParsing
+//
+//  Created by ChoiJinYoung on 3/6/16.
+//  Copyright © 2016 appstamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WeatherCell : UITableViewCell
+
+@property (strong, nonatomic) IBOutlet UIImageView *imgView;
+@property (strong, nonatomic) IBOutlet UILabel *countryLabel;
+@property (strong, nonatomic) IBOutlet UILabel *weatherLabel;
+@property (strong, nonatomic) IBOutlet UILabel *temperatureLabel;
+
+@end
