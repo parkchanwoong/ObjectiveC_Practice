f17cb103e8432f7f74727a1f70190e6b52309afe
Chanwoong Park <foodi1121@gmail.com>
2017-09-22 15:44:35 +0900

    Add JSONParsing

---
+++ Source/JSONParsing/JSONParsing/ViewController.h
@@ -0,0 +1,21 @@
+//
+//  ViewController.h
+//  JSONParsing
+//
+//  Created by ChoiJinYoung on 3/6/16.
+//  Copyright © 2016 appstamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController<UITableViewDataSource,UITableViewDelegate>{
+    
+    NSDictionary *datalist;
+    NSArray *local;
+    
+    
+}
+
+
+@end
+
